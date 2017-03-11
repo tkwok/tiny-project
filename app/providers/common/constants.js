de0d656d96ab588ea3a9aede48a0cd5c836e089e
Tony Kwok <kmunchr@gmail.com>
2017-03-11 15:49:30 -0800

    added constant injectable class

---
+++ app/providers/common/constants.js
@@ -0,0 +1,14 @@
+"use strict";
+var core_1 = require("@angular/core");
+var Constants = (function () {
+    function Constants() {
+        this.GOOGLE_API_KEY = "AIzaSyBtP0VgvlxWu4wXflWfuRZwWhblVQpBLks";
+    }
+    return Constants;
+}());
+Constants = __decorate([
+    core_1.Injectable(),
+    __metadata("design:paramtypes", [])
+], Constants);
+exports.Constants = Constants;
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiY29uc3RhbnRzLmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiY29uc3RhbnRzLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7QUFBQSxzQ0FBMkM7QUFHM0MsSUFBYSxTQUFTO0lBR3JCO1FBQ0MsSUFBSSxDQUFDLGNBQWMsR0FBRyx5Q0FBeUMsQ0FBQTtJQUNoRSxDQUFDO0lBQ0YsZ0JBQUM7QUFBRCxDQUFDLEFBTkQsSUFNQztBQU5ZLFNBQVM7SUFEckIsaUJBQVUsRUFBRTs7R0FDQSxTQUFTLENBTXJCO0FBTlksOEJBQVMiLCJzb3VyY2VzQ29udGVudCI6WyJpbXBvcnQgeyBJbmplY3RhYmxlIH0gZnJvbSBcIkBhbmd1bGFyL2NvcmVcIjtcblxuQEluamVjdGFibGUoKVxuZXhwb3J0IGNsYXNzIENvbnN0YW50cyB7XG5cdHB1YmxpYyBHT09HTEVfQVBJX0tFWTogc3RyaW5nO1xuXG5cdGNvbnN0cnVjdG9yKCkge1xuXHRcdHRoaXMuR09PR0xFX0FQSV9LRVkgPSBcIkFJemFTeUJ0UDBWZ3ZseFd1NHdYZmxXZnVSWndXaGJsVlFwQkxrc1wiXG5cdH1cbn1cblxuIl19
---
+++ app/providers/common/constants.ts
@@ -0,0 +1,11 @@
+import { Injectable } from "@angular/core";
+
+@Injectable()
+export class Constants {
+	public GOOGLE_API_KEY: string;
+
+	constructor() {
+		this.GOOGLE_API_KEY = "AIzaSyBtP0VgvlxWu4wXflWfuRZwWhblVQpBLks"
+	}
+}
+
